68ef7c786fb5de2717ec2ce34e43edd8b70711b2
unknown <nguyen.michl@gmail.com>
2020-03-05 21:30:10 -0600

    Ensure API proxy functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          This is MY APP!!!!
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {wheels: []}
+
+  // Testing get all wheel data on-load. Proxy functional!
+  componentDidMount() {
+    fetch('/wheels')
+      .then(res => res.json())
+      .then(wheels => this.setState({ wheels }));
+  }
+
+  render() {
+    return (
+      <div>
+        words
+      </div>
+    );
+  }
 }
 
 export default App;
