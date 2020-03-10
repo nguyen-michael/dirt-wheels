05071ceb95a38bace335d3677736ae3a9802a4ec
unknown <nguyen.michl@gmail.com>
2020-03-09 19:16:18 -0500

    Revise build and icons

---
+++ build/precache-manifest.e56132820bf59ef058188485026ba340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a73f35c7844c6238ada117fd467ee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eea7f50d50c124906610",
+    "url": "/static/css/main.fc309ee2.chunk.css"
+  },
+  {
+    "revision": "a939ba4e24e2fd41100d",
+    "url": "/static/js/2.39b3b4a4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.39b3b4a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eea7f50d50c124906610",
+    "url": "/static/js/main.c52c2dbc.chunk.js"
+  },
+  {
+    "revision": "9a0bd75a471a289ca9f7",
+    "url": "/static/js/runtime-main.ea84c50e.js"
+  }
+]);
